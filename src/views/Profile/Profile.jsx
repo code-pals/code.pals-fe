c71a71691a3fe1b82e577eb9156f1a6fa0d2ff07
Estefani <stef45689@gmail.com>
2022-02-23 16:29:26 -0600

    Base Layouts Done w/out testing

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -6,15 +6,23 @@ import {
   Text,
   VStack,
   useBreakpointValue,
+  Container,
 } from '@chakra-ui/react';
 
 import CodeBox from '../../components/CodeBox/CodeBox.jsx';
+import GithubBox from '../../components/GithubBox/GithubBox.jsx';
 
 export default function Profile() {
   return (
     <>
-      <CodeBox />
-      <CodeBox />
+      <Container mt="10px" centerContent>
+        <h1> Preferred Name </h1>
+        <h3> Pronouns </h3>
+        <h3> Years of Experience </h3>
+        <h2> Tech Stack </h2>
+      </Container>
+      <CodeBox /> <CodeBox />
+      <GithubBox />
     </>
   );
 }
