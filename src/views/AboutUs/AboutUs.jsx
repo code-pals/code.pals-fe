5cbbbc014a58fd996bac62faa42e2feb4beebc8b
Estefani <stef45689@gmail.com>
2022-02-23 14:34:37 -0600

    Layouts updated

--- src/views/AboutUs/AboutUs.jsx
+++ src/views/AboutUs/AboutUs.jsx
@@ -1,22 +1,15 @@
-import {
-  Stack,
-  Flex,
-  Button,
-  Text,
-  VStack,
-  useBreakpointValue,
-} from '@chakra-ui/react';
+import ElijahBox from '../../components/ElijahBox/ElijahBox.jsx';
+import AmitBox from '../../components/AmitBox/AmitBox.jsx';
+import MarvBox from '../../components/MarvBox/MarvBox.jsx';
+import StefBox from '../../components/StefBox/StefBox.jsx';
 
 export default function AboutUs() {
   return (
-    <Flex
-      w={'full'}
-      h={'100vh'}
-      backgroundImage={
-        'https://images.unsplash.com/photo-1562577308-c8b2614b9b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80'
-      }
-      backgroundSize={'cover'}
-      backgroundPosition={'center center'}
-    ></Flex>
+    <>
+      <ElijahBox />;
+      <AmitBox />
+      <MarvBox />
+      <StefBox />
+    </>
   );
 }
