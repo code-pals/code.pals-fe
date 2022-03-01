341aaf3d5b52aff00896a2f454e3b5c0fb1742a4
ProsperieEli <ElijahProsperie@gmail.com>
2022-03-01 16:13:43 -0500

    storing

--- src/views/BoardDetails/BoardDetails.jsx
+++ src/views/BoardDetails/BoardDetails.jsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { getBoardById } from '../../services/fetch-utils.js';
 import BoardHomeBox from '../../components/BoardHomeBox/BoardHomeBox.jsx';
+import { useUser } from '../../context/UserContext.js';
 
 export default function BoardDetails() {
   const { id } = useParams();
+  const { user } = useUser();
 
   const [board, setBoard] = useState('');
   const [loading, setLoading] = useState(true);
@@ -22,7 +24,7 @@ export default function BoardDetails() {
   return (
     <>
       <BoardHomeBox board={board} goals={board.goal} />
-      <Link href={`/chat/${board.title}`}>
+      <Link to={`/chat/${board.title}/${user.github}`}>
         <Button>Enter Boardroom!</Button>
       </Link>
       {/* {board.title}
