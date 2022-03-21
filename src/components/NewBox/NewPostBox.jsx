c75de915a09bb1e47a3ab5664c90b0baa7ba490c
Frank Hollywood <github@srvsync.com>
2022-03-21 15:00:50 -0700

    made changes to new post box

--- src/components/NewBox/NewPostBox.jsx
+++ src/components/NewBox/NewPostBox.jsx
@@ -7,65 +7,86 @@ import {
   useColorModeValue,
   Text,
   Avatar,
+  Spacer,
 } from '@chakra-ui/react';
-import Highlight from 'react-highlight';
+// import Highlight from 'react-highlight';
 import { Link } from 'react-router-dom';
-import './NewBox.css';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { monokai } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+// import './NewBox.css';
+
+function formatCodeHeight(code, codeHeight) {
+  let displayCode;
+  const splitCode = code.split('\n');
+
+  if (splitCode.length < codeHeight) {
+    const blankSpace = new Array(codeHeight - splitCode.length + 1).fill('');
+    displayCode = splitCode.concat(blankSpace);
+  } else {
+    displayCode = splitCode.slice(0, codeHeight);
+  }
+
+  return displayCode.join('\n');
+}
 
 export default function NewPostBox({ post }) {
   return (
-    <Center>
-      <Link to={`/postdetails/${post.postId}`}>
-        <Box
-          mr="20px"
-          mb="10px"
-          w="300px"
-          h="475px"
-          bg={useColorModeValue('white', 'gray.900')}
-          boxShadow={'2xl'}
-          rounded={'md'}
-          overflow={'hidden'}
-        >
-          <Box h="250px" w="300px">
-            <Highlight>{post?.code?.slice(0, 50)}</Highlight>
-          </Box>
-          <Box h={'75px'} w={'100%'}>
-            <Center>
-              <Heading
-                ml="5px"
-                mr="5px"
-                color={useColorModeValue('gray.700', 'white')}
-                fontSize={'xl'}
-                fontFamily={'body'}
-              >
-                {post?.title?.slice(0, 55)}
-              </Heading>
-            </Center>
-          </Box>
-          <Box>
-            <Center>
-              <Text pt="15px" color={'gray.500'}>
-                {post?.question?.slice(0, 55)}
-              </Text>
-            </Center>
-            <Box>
-              <Stack
-                mt={6}
-                direction={'row'}
-                spacing={4}
-                align={'center'}
-                style={{ margin: 0, padding: 10 }}
-              >
-                <Link to={`/profile/${post.github}`}>
-                  <Avatar src={post?.avatar} alt={'Author'} />
-                  <Text fontWeight={600}>{post.github}</Text>
-                  <Text color={'gray.500'}>{post?.created?.slice(0, 10)}</Text>
-                </Link>
-              </Stack>
-            </Box>
-          </Box>
+    // <Center>
+    <Link to={`/postdetails/${post.postId}`}>
+      <Box
+        mr="20px"
+        mb="10px"
+        w="300px"
+        h="475px"
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        overflow={'hidden'}
+      >
+        <Box maxH="250px" w="300px">
+          <SyntaxHighlighter style={monokai}>
+            {formatCodeHeight(post?.code, 8)}
+          </SyntaxHighlighter>
         </Box>
-      </Link>
-    </Center>
+        <Box h={'75px'} w={'100%'}>
+          {/* <Center> */}
+          <Heading
+            ml="5px"
+            mr="5px"
+            mt="15px"
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'xl'}
+            fontFamily={'body'}
+          >
+            {formatCodeHeight(post?.title, 5)}
+          </Heading>
+          {/* </Center> */}
+        </Box>
+        {/* <Box> */}
+        {/* <Box> */}
+        <Stack
+          mt={6}
+          direction={'column'}
+          spacing={4}
+          align={'left'}
+          style={{ margin: 0, padding: 10 }}
+        >
+          {/* <Center> */}
+          <Text pt="15px" color={'gray.500'}>
+            {post?.question?.slice(0, 55)}
+          </Text>
+          {/* </Center> */}
+          <Spacer />
+          <Link to={`/profile/${post.github}`}>
+            <Avatar src={post?.avatar} alt={'Author'} />
+            <Text fontWeight={600}>{post.github}</Text>
+            <Text color={'gray.500'}>{post?.created?.slice(0, 10)}</Text>
+          </Link>
+        </Stack>
+        {/* </Box> */}
+      </Box>
+      {/* </Box> */}
+    </Link>
+    // </Center>
   );
 }
