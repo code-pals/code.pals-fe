5bf59b711947ec9288ece625eb9ec724d5c0c255
AmitSheth9 <amit.sheth9@gmail.com>
2022-03-23 14:50:07 -0500

    changed urls to heroku

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,7 +1,7 @@
 import request from 'superagent';
 
-const url = 'http://localhost:7890/api/v1';
-//const url = 'https://codepalz.herokuapp.com/api/v1';
+//const url = 'http://localhost:7890/api/v1';
+const url = 'https://codepalz.herokuapp.com/api/v1';
 
 export async function getAllPosts() {
   const response = await request.get(`${url}/posts`);
